9d7c0dbae5a6c33a3f26be4b7bbe1370bb828619
filip <filip0822@gmail.com>
2019-10-27 23:01:38 +0100

    async test fix

--- src/_Tests/TemperatureSensor.test.ts
+++ src/_Tests/TemperatureSensor.test.ts
@@ -2,15 +2,15 @@ import 'reflect-metadata'
 
 
 import { Mock, It } from 'moq.ts'
-import TemperatureSensor, { IHttp } from '../TemperatureSensor';
+import TemperatureSensor from '../TemperatureSensor';
 import { WeatherApi } from '../WeatherApi';
 describe('TemperatureSensor tests', () => {
-   it('should convert Celcius to Fahrenheit', () => {
+   it('should convert Celcius to Fahrenheit', async () => {
        const weatherApiMock = new Mock<WeatherApi>();
        weatherApiMock.setup(m => m.getTemperature()).returns(5);
        const unitUnderTest: TemperatureSensor = new TemperatureSensor(weatherApiMock.object());
        
-       let result: Promise<number> = unitUnderTest.getTemperature();
+       let result: number = await unitUnderTest.getTemperature();
        
        expect(result).toBe(8.5);
    })
